5990581f8267ed7ca58412935e77fcf6f6918abe
Vic Putz <vbputz@gmail.com>
2017-04-25 16:35:14 -0600

    sphere getting closer

--- README.md
+++ README.md
@@ -1,20 +1,9 @@
-# boot-cljs-example [![Build Status][badge]][build]
+# qtut
 
-Example project using [the boot build tool][boot] with the [boot-cljs],
-[boot-cljs-repl], and [boot-reload] tasks.
+Example project playing with Clojurescript, boot, and various visualizations to
+make an interactive quantum computer simulator.
 
-## Prepare
-
-[Install boot][installboot].  Then, in a terminal:
-
-```bash
-boot -u
-```
-
-This will update boot to the latest stable release version. Since boot is
-pre-release software at the moment, you should do this frequently.
-
-## Build
+## Development
 
 In a terminal do:
 
@@ -46,24 +35,6 @@ You can view the generated content by opening
 [http://localhost:3000/index.html](http://localhost:3000/index.html)
 in your browser.
 
-> *OutOfMemoryError Troubleshooting*
->
-> boot provides a tool called _pods_ that make it possible for multiple
-> independent Clojure classpaths to exist in the same JVM.  Task authors
-> can use Maven dependencies without worrying about
-> shadowing or otherwise interfering with the dependencies in other pods.
->
-> One downside of pods is that their use results in higher-than-usual
-> memory consumption by the JVM, particularly
-> [PermGen](http://stackoverflow.com/questions/88235/dealing-with-java-lang-outofmemoryerror-permgen-space-error).
->
-> If you are using Java 7, you may see errors related to PermGen.  You
-> can consult the
-> [JVM Options](https://github.com/boot-clj/boot/wiki/JVM-Options) wiki
-> page for settings that can help.
->
-> You may also consider upgrading to Java 8, as it
-> [resolves many PermGen-related issues](http://www.infoq.com/news/2013/03/java-8-permgen-metaspace).
 
 ## Start Browser REPL
 
@@ -84,17 +55,8 @@ Reload the page in your browser.  Your REPL is now connected to the page.
 
 ## License
 
-Copyright © 2014 Adzerk
+Copyright © 2017 VPutz
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
 
-[badge]:            https://travis-ci.org/adzerk-oss/boot-cljs-example.png?branch=master
-[build]:            https://travis-ci.org/adzerk-oss/boot-cljs-example
-[boot]:             https://github.com/boot-clj/boot
-[cider]:            https://github.com/clojure-emacs/cider
-[boot-cljs]:        https://github.com/adzerk/boot-cljs
-[boot-cljs-repl]:   https://github.com/adzerk/boot-cljs-repl
-[boot-reload]:      https://github.com/adzerk/boot-reload
-[installboot]:      https://github.com/boot-clj/boot#install
-[gclosure]:         https://developers.google.com/closure/compiler/
--- src/app/views.cljs
+++ src/app/views.cljs
@@ -21,7 +21,10 @@
       :component-did-mount
       (fn three-canvas-did-mount [this]
         (let [e (reagent/dom-node this)
-              r (create-renderer e)]
+              r (doto (create-renderer e)
+                  (.setClearColor 0xffffff 1.0))
+                                        ;              e (js/THREE.OutlineEffect. r)
+              ]
           ((fn animate []
              (tick)
              (.render r scene camera)
@@ -36,22 +39,96 @@
       (-> (.-position) (.set (* 100 (js/Math.cos t)) (* 100 (js/Math.sin t)) 100))
       (.lookAt (.-position scene)))))
 
+(defn bloch-arrow
+  [phi theta radius]
+  (let [dir (doto (js/THREE.Vector3. (* (.cos js/Math phi) (.sin js/Math theta))
+                                     (* (.sin js/Math phi) (.sin js/Math theta))
+                                     (.cos js/Math theta))
+              (.normalize))
+        origin (js/THREE.Vector3. 0 0 0)
+        color 0x000000
+        arrow (js/THREE.ArrowHelper. dir origin radius color)]
+    arrow))
+
+(defn bloch-sphere-group
+  [radius]
+  (let [
+        axis-radius (+ radius 2)
+        widthsegments 50
+        heightsegments 50
+        axis-opacity 0.7
+        sphere-opacity 0.1
+        xplane (js/THREE.CircleBufferGeometry. axis-radius widthsegments)
+        yplane (doto (js/THREE.CircleBufferGeometry. axis-radius widthsegments)
+                 (.rotateY (/ 3.14 2)))
+        zplane (doto (js/THREE.CircleBufferGeometry. axis-radius widthsegments)
+                 (.rotateX (/ 3.14 2)))
+        sphere (js/THREE.SphereBufferGeometry. radius widthsegments heightsegments)
+        smaterial (js/THREE.MeshBasicMaterial. #js {:color 0x000000
+                                                    :wireframe false
+                                                    :wireframeLinewidth 0.5
+                                                    :transparent true
+                                                    :side js/THREE.DoubleSide
+                                                    :opacity sphere-opacity})
+        x-material (js/THREE.MeshToonMaterial. #js {:color "blue"
+                                                    :wireframe false
+                                                    :transparent true
+                                                    :side js/THREE.DoubleSide
+                                                    :opacity axis-opacity})
+        y-material (js/THREE.MeshToonMaterial. #js {:color "red"
+                                                    :wireframe false
+                                                    :transparent true
+                                                    :side js/THREE.DoubleSide
+                                                    :opacity axis-opacity})
+        z-material (js/THREE.MeshToonMaterial. #js {:color 0x00ff00;"green"
+                                                    :wireframe false
+                                                    :transparent true
+                                                    :side js/THREE.DoubleSide
+                                                    :opacity axis-opacity})
+        smesh (js/THREE.Mesh. sphere smaterial)
+        xplane-mesh (js/THREE.Mesh. xplane x-material)
+        yplane-mesh (js/THREE.Mesh. yplane y-material)
+        zplane-mesh (js/THREE.Mesh. zplane z-material)]
+    (doto (js/THREE.Group.)
+      (.add xplane-mesh)
+      (.add yplane-mesh)
+      (.add zplane-mesh)
+      (.add smesh)
+      )
+    ))
+
 (defn bloch-sphere-component
   "A bloch sphere represented in Three.js"
   [alpha beta]
   (let [
-        geometry (js/THREE.BoxBufferGeometry. 50 50 50)
-        material (js/THREE.MeshBasicMaterial. #js {:color "green" :wireframe true})
-        mesh (js/THREE.Mesh. geometry material)
+        radius 50
+        viewdist 150
+        camphi (/ (.-PI js/Math) 4)
+        camtheta (/ (.-PI js/Math) 3)
+        arrow-phi (/ (.-PI js/Math) 2)
+        arrow-theta (/ (.-PI js/Math) 4)
+        sphere        (bloch-sphere-group radius)
+        arrow (bloch-arrow arrow-phi arrow-theta radius)
         scene (doto (js/THREE.Scene.)
                 (.add (js/THREE.AmbientLight. 0x888888))
                 (.add (doto (js/THREE.DirectionalLight. 0xffff88 0.5)
                         (-> (.-position) (.set -600 300 600))))
-                (.add (js/THREE.AxisHelper. 50))
-                (.add mesh))
-        camera (js/THREE.PerspectiveCamera. 70 1 1 1000)
+                (.add (js/THREE.AxisHelper. 50)) ; x red y green z blue
+                (.add arrow)
+                (.add sphere)
+                )
+        camera (doto (js/THREE.PerspectiveCamera. 70 1 1 1000)
+                 (-> (.-position) (.set (* viewdist (* (js/Math.cos camphi)
+                                                     (js/Math.sin camtheta)) )
+                                      (* viewdist (* (js/Math.sin camphi)
+                                                     (js/Math.sin camtheta)))
+                                      (* viewdist (js/Math.cos camtheta))))
+                 (-> (.-up) (.set 0 0 1))
+
+                 (.lookAt (.-position scene)))
         tick (fn []
-               (fly-around-z-axis camera scene))]
+                                        ; (fly-around-z-axis camera scene)
+               )]
     [three-canvas {:width 500 :height 500} camera scene tick]))
 
 
